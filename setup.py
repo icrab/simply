0ae868a867c76f2cec89f895c8f9e0def9dd8337
Sergey Sosnin <serg.sosnin@gmail.com>
2020-05-23 12:05:51 +0300

    added code

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='simply',
+      version='0.1',
+      description='Simple RPC system for the great good',
+      author='Sergey Sosnin',
+      author_email='serg.sosnin@gmail.com',
+      packages=['simply'],
+      install_requires=[
+          'websockets',
+          'msgpack',
+          'pebble',
+          'asyncio'
+      ]
+     )
---
+++ simply/__init__.py
@@ -0,0 +1,3 @@
+from simply.simplyRPCServer import SimplyRPCServer
+__all__ = ['SimplyRPCServer']
+__version__ = "0.0.1"
