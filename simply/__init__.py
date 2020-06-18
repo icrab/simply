e2d2a1bcc5e2fbe40076d95616af50b37712b770
Sergey Sosnin <serg.sosnin@gmail.com>
2020-06-18 12:18:48 +0300

    Added Redis server

--- simply/__init__.py
+++ simply/__init__.py
@@ -1,3 +1,5 @@
-from simply.simplyRPCServer import SimplyRPCServer
-__all__ = ['SimplyRPCServer']
+from .simplyRPCServer import SimplyRPCServer,SimplyRedisServer
+#from .simplyRPClient import simplyRPClient
+
+__all__ = ['SimplyRPCServer','SimplyRedisServer']
 __version__ = "0.0.1"
