fa68acb1ce90a1276a10e5ce5074240e8773cc8a
Sergey Sosnin <serg.sosnin@gmail.com>
2020-06-29 17:57:25 +0300

    only for debug

--- simply/simplyRPClient.py
+++ simply/simplyRPClient.py
@@ -12,15 +12,15 @@ class SimplyRedisClient():
         idx = str(uuid.uuid4())
         run = {'method': function, 'type': type, 'args': args, 'kwargs': kwargs, 'id': idx}
         self.redis.rpush('{}:{}'.format(self.name,self.plugin), msgpack.packb(run, use_bin_type=True))
-        res = msgpack.unpackb(self.redis.blpop('{}:general:{}'.format(self.name,idx))[1])
+        res = msgpack.unpackb(self.redis.blpop('{}:general:{}'.format(self.name,idx))[1],raw=False)
         #print('first ',res)
         if type == 'delayed':
         #    print(res)
-            res = msgpack.unpackb(self.redis.blpop('{}:general:{}'.format(self.name,idx))[1])
+            res = msgpack.unpackb(self.redis.blpop('{}:general:{}'.format(self.name,idx))[1],raw=False)
         print("res is ", res)
-        if res[b'status'] == b'error':
-            raise Exception(res[b'exception'])
-        elif res[b'status'] == b'ok':
-            return res[b'result']
+        if res['status'] == 'error':
+            raise Exception(res['exception'])
+        elif res['status'] == 'ok':
+            return res['result']
         else:
             raise Exception("Unknown error: {}".format(res))
