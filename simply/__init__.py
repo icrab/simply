29d584830188e216498b08c28e2728720f350b33
Sergey Sosnin <serg.sosnin@gmail.com>
2020-10-01 14:40:08 +0300

    Update __init__.py

--- simply/__init__.py
+++ simply/__init__.py
@@ -1,4 +1,4 @@
-from .simplyRPCServer import SimplyRPCServer,SimplyRedisServer
+from .simplyRPCServer import SimplyRedisServer
 from .simplyRPClient import SimplyRedisClient
-__all__ = ['SimplyRPCServer','SimplyRedisServer','SimplyRedisClient']
+__all__ = ['SimplyRedisServer','SimplyRedisClient']
 __version__ = "0.0.2"
