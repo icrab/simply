28ed901c2948a1b08b1cc94d2d335e7b2d428abd
icrab <icrab93@gmail.com>
2022-10-15 14:45:08 +0500

    fix: fixed publish error message

--- simply/simplyRPCServer.py
+++ simply/simplyRPCServer.py
@@ -204,8 +204,10 @@ class SimplyRedisServer():
             try:
                 self.redis.publish("{}:general:{}".format(self.name, call['id']),
                                    msgpack.packb(result, use_bin_type=True))
-            except:
+            except Exception as e:
                 time.sleep(1)
+                self.logger.critical(
+                    "ERROR", str(e))
                 self.logger.critical(
                     "Connection to Redis failed during writing, trying to reconnect")
 
